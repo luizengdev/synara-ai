8661d1b9e5915da442c8a31fa438c6b1bc72eec6
luizengdev <luizengdev@gmail.com>
2024-02-16 13:24:33 -0300

    feat: add pathname to link navigation sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Montserrat } from "next/font/google";
+import { usePathname } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import {
@@ -46,7 +47,7 @@ const routes = [
     color: "text-orange-700",
   },
   {
-    label: "Geração de Musica",
+    label: "Geração de Música",
     icon: Music,
     href: "/music",
     color: "text-emerald-500",
@@ -65,6 +66,8 @@ const routes = [
 ];
 
 const Sidebar = () => {
+  const pathname = usePathname();
+
   return (
     <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
       <div className="px-3 py-2 flex-1">
@@ -81,9 +84,12 @@ const Sidebar = () => {
             <Link
               href={route.href}
               key={route.href}
-              className="text-sm group flex p-3 w-full
-              justify-start font-medium cursor-pointer
-              hover:text-white hover:bg-white/10 rounded-lg transition"
+              className={cn(
+                "text-sm group flex p-3 w-full justify-start font-medium cursor-pointerh hover:text-white hover:bg-white/10 rounded-lg transition",
+                pathname === route.href
+                  ? "text-white bg-white/10"
+                  : "text-zinc-400"
+              )}
             >
               <div className="flex items-center flex-1">
                 <route.icon className={cn("w-5 h-5 mr-3", route.color)} />
