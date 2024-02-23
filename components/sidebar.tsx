dc78bbf3eecfc4d1445ee0053ae7aa87c29b118f
luizengdev <luizengdev@gmail.com>
2024-02-23 16:30:44 -0300

    feat: add apiLimitCount to layout and sidebar

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,6 +15,7 @@ import {
   Settings,
   VideoIcon,
 } from "lucide-react";
+import { FreeCounter } from "@/components/free-counter";
 
 const montserrat = Montserrat({
   weight: "600",
@@ -65,7 +66,11 @@ const routes = [
   },
 ];
 
-const Sidebar = () => {
+interface SidebarProps {
+  apiLimitCount: number;
+}
+
+const Sidebar = ({ apiLimitCount = 0 }: SidebarProps) => {
   const pathname = usePathname();
 
   return (
@@ -99,6 +104,7 @@ const Sidebar = () => {
           ))}
         </div>
       </div>
+      <FreeCounter apiLimitCount={apiLimitCount} />
     </div>
   );
 };
