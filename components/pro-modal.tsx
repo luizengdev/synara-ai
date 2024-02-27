d54fbed2ea918514571fe7c45c163e8725d1adc4
luizengdev <luizengdev@gmail.com>
2024-02-27 18:09:19 -0300

    feat: add onClick sonSubscribe axios get

--- components/pro-modal.tsx
+++ components/pro-modal.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import axios from "axios";
+import { useState } from "react";
+
 import {
   Check,
   Code,
@@ -60,6 +63,20 @@ const tools = [
 
 export const ProModal = () => {
   const proModal = UseProModal();
+  const [loading, setLoading] = useState(false);
+
+  const onSubscribe = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get("/api/stripe");
+
+      window.location.href = response.data.url;
+    } catch (error) {
+      console.log(error, "STRIPE_CLIENT_ERROR");
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -94,7 +111,12 @@ export const ProModal = () => {
           </DialogDescription>
         </DialogHeader>
         <DialogFooter>
-          <Button size={"lg"} variant={"premium"} className="w-full">
+          <Button
+            onClick={onSubscribe}
+            size={"lg"}
+            variant={"premium"}
+            className="w-full"
+          >
             Upgrade
             <Zap className="w-4 h-4 ml-2 fill-white" />
           </Button>
