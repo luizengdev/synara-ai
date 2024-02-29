832f64bd11431dc4ca33278aa6a21a64c01e7321
luizengdev <luizengdev@gmail.com>
2024-02-29 15:13:30 -0300

    feat: add LandingNavbar, LandingHero and LandingContent to Landing page

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,19 +1,13 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import React from "react";
+import { LandingContent } from "@/components/landing-content";
+import { LandingHero } from "@/components/landing-hero";
+import { LandingNavbar } from "@/components/landing-navbar";
 
 const LandingPage = () => {
   return (
-    <div>
-      Landing Page (Unprotected)
-      <div>
-        <Link href="/sign-in">
-          <Button>Entrar</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button>Cadastre-se</Button>
-        </Link>
-      </div>
+    <div className="h-full">
+      <LandingNavbar />
+      <LandingHero />
+      <LandingContent />
     </div>
   );
 };
